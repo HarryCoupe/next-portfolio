d6d7d97789d283d2b476d3e2401bd0064ead5bbe
Harry Coupe <psyhc5@nottingham.ac.uk>
2023-04-06 09:21:54 +0100

    footer button css inject order fix

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,4 +1,4 @@
-import { IconButton } from "@mui/material";
+import { IconButton, StylesProvider } from "@mui/material";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
@@ -11,29 +11,39 @@ export default function Footer() {
   return (
     <div className="footer">
       <a href={GitHub} target="_blank" aria-label="Github Link">
-        <IconButton size="large" className="buttonColor" aria-label="Github">
-          <GitHubIcon fontSize="large" />
-        </IconButton>
+        <StylesProvider injectFirst>
+          <IconButton
+            size="large"
+            classes={{ contained: "buttonColor" }}
+            aria-label="Github"
+          >
+            <GitHubIcon fontSize="large" />
+          </IconButton>
+        </StylesProvider>
       </a>
 
       <a href={Twitter} target="_blank" aria-label="Twitter Link">
-        <IconButton
-          size="large"
-          className="buttonColor"
-          aria-label="Twitter button"
-        >
-          <TwitterIcon fontSize="large" />
-        </IconButton>
+        <StylesProvider injectFirst>
+          <IconButton
+            size="large"
+            className="buttonColor"
+            aria-label="Twitter button"
+          >
+            <TwitterIcon fontSize="large" />
+          </IconButton>
+        </StylesProvider>
       </a>
 
       <a href={LinkedIn} target="_blank" aria-label="LinkedIn Link">
-        <IconButton
-          size="large"
-          className="buttonColor"
-          aria-label="LinkedIn button"
-        >
-          <LinkedInIcon fontSize="large" />
-        </IconButton>
+        <StylesProvider injectFirst>
+          <IconButton
+            size="large"
+            className="buttonColor"
+            aria-label="LinkedIn button"
+          >
+            <LinkedInIcon fontSize="large" />
+          </IconButton>
+        </StylesProvider>
       </a>
     </div>
   );
